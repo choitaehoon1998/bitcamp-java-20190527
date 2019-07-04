e92e4edb242d42d20018c36927e5648d39d77b20
choitaehoon1998 <hugebigdream@gmail.com>
2019-07-04 17:52:30 +0900

    bitcamp-java-application v04 완료

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
@@ -1,13 +1,42 @@
 package com.eomcs.lms;
 
+import java.util.Scanner;
+
 public class App2 {
-  public static void main(String []args) {
-    System.out.println("번호: 1");
-    System.out.println("이름: 홍길동");
-    System.out.println("이메일: hong@test.com");
-    System.out.println("암호: 1111");
-    System.out.println("사진: hong.png");
-    System.out.println("전화: 1111-2222");
-    System.out.println("가입일: 2019-01-01");
+  public static void main(String[] args) {
+
+    java.io.InputStream keyboard = System.in;
+    Scanner keyscan = new Scanner(keyboard);
+    
+    System.out.print("번호는?");
+    String num = keyscan.nextLine();
+    
+    System.out.print("이름은?");
+    String name = keyscan.nextLine();
+    
+    System.out.print("이메일은?");
+    String email = keyscan.nextLine();
+    
+    System.out.print("비밀번호는?");
+    String password = keyscan.nextLine();
+    
+    System.out.print("사진은?");
+    String photo = keyscan.nextLine();
+    
+    System.out.print("전화번호는?");
+    String phoneNumber = keyscan.nextLine();
+    
+    System.out.print("가입일자는?");
+    String joinDate = keyscan.nextLine();
+    
+    System.out.println();
+    
+    System.out.println("번호: " + num);
+    System.out.println("이름: " +name);
+    System.out.println("이메일: "+email);
+    System.out.println("암호: "+password);
+    System.out.println("사진: "+photo);
+    System.out.println("전화: "+phoneNumber);
+    System.out.println("가입일: "+joinDate);
   }
 }
--- bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
@@ -1,10 +1,29 @@
 package com.eomcs.lms;
 
+import java.util.Scanner;
+
 public class App3 {
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("내용: 게시글입니다.");
-    System.out.println("작성일: 2019-01-01");
-    System.out.println("조회수: 0");
+    java.io.InputStream keyboard = System.in;
+    Scanner keyscan = new Scanner(keyboard);
+    
+    System.out.print("번호는?");
+    String number = keyscan.nextLine();
+    
+    System.out.print("내용은?");
+    String content = keyscan.nextLine();
+    
+    System.out.print("작성일은?");
+    String writeDate = keyscan.nextLine();
+    
+    System.out.print("조회수는?");
+    String visitNum = keyscan.nextLine();
+    
+    System.out.println();
+    
+    System.out.println("번호: "+number);
+    System.out.println("내용: "+content);
+    System.out.println("작성일: "+writeDate);
+    System.out.println("조회수: "+visitNum);
   }
 }
