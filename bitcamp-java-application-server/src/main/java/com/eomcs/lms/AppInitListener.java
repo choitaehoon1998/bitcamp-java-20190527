b3b89716e8fa126c43b4de2a5391592f48c0f2f7
choitaehoon1998 <hugebigdream@gmail.com>
2019-08-06 14:22:04 +0900

    bitcamp-java-application v33_3백업완료

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/AppInitListener.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/AppInitListener.java
@@ -5,10 +5,13 @@ import com.eomcs.lms.context.ServletContextListener;
 import com.eomcs.lms.dao.csv.BoardCsvDao;
 import com.eomcs.lms.dao.csv.LessonCsvDao;
 import com.eomcs.lms.dao.csv.MemberCsvDao;
+import com.eomcs.lms.servlet.BoardServlet;
+import com.eomcs.lms.servlet.LessonServlet;
+import com.eomcs.lms.servlet.MemberServlet;
 
 // 서버가 시작되거나 종료될 때 보고를 받고 작업을 수행하는 역할
 // => ServletContextListener 규칙을 준비해야만 서버의 시작과 종료 알림을 받을 수 있다.
-public class ApplicationInitListener implements ServletContextListener {
+public class AppInitListener implements ServletContextListener {
   
   BoardCsvDao boardDao; 
   MemberCsvDao memberDao;
@@ -21,32 +24,27 @@ public class ApplicationInitListener implements ServletContextListener {
     
     try {
       boardDao = new BoardCsvDao("./board.csv");
-      //boardDao = new BoardSerialDao("./board.ser");
-      context.put("boardDao", boardDao);
-      
     } catch (Exception e) {
       System.out.println("게시물 데이터 로딩 중 오류 발생!");
     }
     
     try {
       memberDao = new MemberCsvDao("./member.csv");
-      //memberDao = new MemberSerialDao("./member.ser");
-      context.put("memberDao", memberDao);
-      
     } catch (Exception e) {
       System.out.println("회원 데이터 로딩 중 오류 발생!");
     }
     
     try {
       lessonDao = new LessonCsvDao("./lesson.csv");
-      //lessonDao = new LessonSerialDao("./lesson.ser");
-      context.put("lessonDao", lessonDao);
-      
     } catch (Exception e) {
       System.out.println("수업 데이터 로딩 중 오류 발생!");
     }
-    
+    context.put("/board/",new BoardServlet(boardDao));
+    context.put("/lesson/",new LessonServlet(lessonDao));
+    context.put("/member/",new MemberServlet(memberDao));
   }
+  
+ 
   
   @Override
   public void contextDestroyed(Map<String, Object> context) {
