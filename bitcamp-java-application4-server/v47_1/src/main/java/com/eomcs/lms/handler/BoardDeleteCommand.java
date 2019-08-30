799f5cce314ab091d4c742e12a486ca365a2eb11
choitaehoon1998 <hugebigdream@gmail.com>
2019-08-30 21:58:32 +0900

    bi-ja-app-v47_1

---
+++ bitcamp-java-application4-server/v47_1/src/main/java/com/eomcs/lms/handler/BoardDeleteCommand.java
@@ -0,0 +1,35 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.util.Component;
+import com.eomcs.util.Input;
+@Component("/board/delete")
+public class BoardDeleteCommand implements Command {
+
+  private BoardDao  boardDao;
+
+  public BoardDeleteCommand(BoardDao  boardDao) {
+    this.boardDao = boardDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in , PrintStream out ) {
+
+    // 사용자가 입력한 번호를 가지고 목록에서 그 번호에 해당하는 Board 객체를 찾는다.
+    try{
+      int no = Input.getIntValue(in,out,"번호? ");
+      if (boardDao.delete(no) > 0) {
+        out.println("데이터삭제");
+      } else {
+        out.println("해당데이터가없습니다");
+      }
+    } catch (Exception e) {
+      out.println("데이터 삭제에 실패했습니다.");
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+}
---
+++ bitcamp-java-application4-server/v47_1/src/main/java/com/eomcs/lms/handler/MemberListCommand.java
@@ -0,0 +1,29 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.List;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.Component;
+@Component("/member/list")
+public class MemberListCommand implements Command{
+  private MemberDao memberDao;
+  
+  @Override
+  public void execute(BufferedReader in , PrintStream out ) {
+   try {
+     List<Member>members = memberDao.findAll();
+     for (Member member :members) { 
+       out.printf("%s,%s,%s,%s,%s\n",
+           member.getNo(),member.getName(),member.getEmail(),member.getRegisteredDate(),
+           member.getTel());
+     }
+   }catch(Exception e) {
+     out.println("데이터 목록 조회에 실패하였습니다.");
+     e.printStackTrace();
+     
+   }
+
+  }
+}
